d9c25230b89ab16d9433622b1bdf4f1f051df1be
petericranston <39384415+petericranston@users.noreply.github.com>
2024-05-16 18:54:47 +0100

    Improving my brick breaker game

--- All Projects/Classwork/Brick Breaker/sketch.js
+++ All Projects/Classwork/Brick Breaker/sketch.js
@@ -1,11 +1,9 @@
 let aDown = false;//Variable to determine if a is down
 let dDown = false;//Variable to determine if is down
 
-let increase = 0;//variable to change the location of the blocks
-
 let ball = {
-  x: 50,
-  y: 50,
+  x: 200,
+  y: 200,
   xSpeed: 3,
   ySpeed: 3,
   d: 10
@@ -38,6 +36,68 @@ function keyReleased() {//Function for to keep the paddle going until released
   }
 }
 
+
+class Bricks {
+  constructor(w, h, x, y) {
+    this.w = w;
+    this.h = h;
+    this.x = x;
+    this.y = y;
+    this.hit = false;
+  }
+
+
+  create() {
+    if (!this.hit) {
+      rect(this.x, this.y, this.w, this.h)
+    }
+  }
+
+}
+
+let blocks = [];
+
+
+function setup() {
+  createCanvas(400, 400);
+  createBlocks();
+}
+
+function draw() {
+  background(220);
+
+  movePaddle();
+
+
+
+
+
+  for (let i = 0; i < blocks.length; i++) {
+    blocks[i].create();
+  }
+
+
+}
+
+
+
+function createBlocks() {
+
+  let rows = 5;
+  let cols = 4;
+  let brickWidth = 100;
+  let brickHeight = 20;
+
+  for (let row = 0; row < rows; row++) {
+    for (let col = 0; col < cols; col++) {
+      let x = col * brickWidth;
+      let y = row * brickHeight;
+      let brick = new Bricks(brickWidth, brickHeight, x, y);
+      blocks.push(brick);
+    }
+  }
+}
+
 function movePaddle() {
   if (aDown) {//If statement to move paddle up if W is down
     paddle.x -= paddle.speed;
@@ -56,54 +116,26 @@ function movePaddle() {
   rect(paddle.x, paddle.y, paddle.w, paddle.h);//Creating paddle
 }
 
-class Bricks {
-  constructor(w, h, x, y) {
-    this.w = w;
-    this.h = h;
-    this.x = x;
-    this.y = y;
-    this.hit = false
+
+
+function moveBall() {//Function for moving the ball
+  if (ball.x > width) {
+    ball.xSpeed *= -1;
+  }
+  else if (ball.x < 10) {
+    ball.xSpeed *= -1;
   }
 
+  ball.x += ball.xSpeed;
 
-  create() {
-    rect(this.x, this.y, this.w, this.h)
+  if (ball.y > height) {
+    ball.ySpeed *= -1;//Changing ball direction when it hits the side
   }
-}
-
-function createBlocks() {
-  brick1 = new Bricks(100, 20, 0, 0);
-  brick2 = new Bricks(100, 20, 100, 0);
-  brick3 = new Bricks(100, 20, 200, 0);
-  brick4 = new Bricks(100, 20, 300, 0);
-
-
-
-
-
-
-  blocks = [brick1, brick2, brick3, brick4];
-
-  for (i = 0; 0 < 4; i++) {
-    blocks[i].create();
+  if (ball.y < 0) {//Changing ball direction when it hits the side
+    ball.ySpeed *= -1;
   }
-}
 
+  ball.y += ball.ySpeed;//Changing ball speed
 
-function setup() {
-  createCanvas(400, 400);
-  createBlocks();
-
-}
-
-function draw() {
-  background(220);
-
-  movePaddle();
-
-
-
-
-
-
+  circle(ball.x, ball.y, ball.d)//Creating ball
 }
