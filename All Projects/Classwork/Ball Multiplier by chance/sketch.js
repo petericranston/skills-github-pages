fe9c1b16f3a3b7a770e5349b7162e6a554f1590f
petericranston <39384415+petericranston@users.noreply.github.com>
2024-05-17 19:25:22 +0100

    creating new program

---
+++ All Projects/Classwork/Ball Multiplier by chance/sketch.js
@@ -0,0 +1,28 @@
+let balls = [];
+
+class ball {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+
+  }
+}
+
+
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+
+  createBall();
+
+}
+
+function createBall() {
+
+  circle(200, 200, 30)
+}
+
+
