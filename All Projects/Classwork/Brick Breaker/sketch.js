174ed844668c5a906cf57acee83ae7c604e0a76a
petericranston <39384415+petericranston@users.noreply.github.com>
2024-05-18 09:37:15 +0100

    Finishing brick breaker game and adding comments

--- All Projects/Classwork/Brick Breaker/sketch.js
+++ All Projects/Classwork/Brick Breaker/sketch.js
@@ -1,7 +1,7 @@
 let aDown = false;//Variable to determine if a is down
 let dDown = false;//Variable to determine if is down
 
-let ball = {
+let ball = {//Container for ball variables
   x: 200,
   y: 200,
   xSpeed: 3,
@@ -9,7 +9,7 @@ let ball = {
   d: 10
 }
 
-let paddle = {
+let paddle = {//Container for paddle variables
   x: 200,
   y: 390,
   speed: 5,
@@ -37,7 +37,7 @@ function keyReleased() {//Function for to keep the paddle going until released
 }
 
 
-class Bricks {
+class Bricks {//class for storing bricks variables to be used to create them
   constructor(w, h, x, y) {
     this.w = w;
     this.h = h;
@@ -47,55 +47,67 @@ class Bricks {
   }
 
 
-  create() {
+  create() {//Creating bricks
     if (!this.hit) {
       rect(this.x, this.y, this.w, this.h)
     }
   }
 
-  checkCollisions() {
+  checkCollisions(ball) {//Checking ball collision on bricks
+    if (
+      !this.hit &&
+      ball.x > this.x &&
+      ball.x < this.x + this.w &&
+      ball.y > this.y &&
+      ball.y < this.y + this.h
+    ) {
+      this.hit = true;
+      ball.ySpeed *= -1;
+    }
 
   }
 }
 
-let blocks = [];
+let blocks = [];//array to store blocks
 
 
-function setup() {
+function setup() {//Setup function
   createCanvas(400, 400);
   createBlocks();
+  randomDirectionBall();
 }
 
 function draw() {
   background(220);
 
 
-
+  //Calling functions
   movePaddle();
   moveBall();
   checkPaddleCollision();
+  lost();
 
 
-  for (let i = 0; i < blocks.length; i++) {
+  for (let i = 0; i < blocks.length; i++) {//Creating blocks from array and checking for collisions
     blocks[i].create();
+    blocks[i].checkCollisions(ball);
   }
-
 
 }
 
 function createBlocks() {
 
-  let rows = 5;
-  let cols = 4;
-  let brickWidth = 100;
-  let brickHeight = 20;
+  let rows = 5;//Setting number of blocks rows
+  let cols = 4;//Setting number of blocks columns
+  let brickWidth = 100;//setting brick width
+  let brickHeight = 20;//Setting brick height
 
-  for (let row = 0; row < rows; row++) {
+  for (let row = 0; row < rows; row++) {//Creating blocks
     for (let col = 0; col < cols; col++) {
       let x = col * brickWidth;
       let y = row * brickHeight;
       let brick = new Bricks(brickWidth, brickHeight, x, y);
-      blocks.push(brick);
+      blocks.push(brick);//Adding to blocks array
 
     }
   }
@@ -153,4 +165,15 @@ function checkPaddleCollision() {
     ball.ySpeed *= -1; // Reverse the ball's y direction if it hits the paddle
     ball.y = paddle.y - ball.d / 2; // Position the ball above the paddle to avoid sticking
   }
+}
+
+function lost() {//Resetting game when the ball hits the bottom
+  if (ball.y > height) {
+    location.reload();
+  }
+}
+
+function randomDirectionBall() {//Setting random ball speed and direction
+  ball.xSpeed = (random(3, 5));
+  ball.ySpeed = (random(3, 5));
 }
