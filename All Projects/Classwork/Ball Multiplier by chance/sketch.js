8c59376a237bf5e1b897486bd2384b53dcbf34f2
petericranston <39384415+petericranston@users.noreply.github.com>
2024-05-19 19:47:50 +0100

    Adding gravity to multiballs project

--- All Projects/Classwork/Ball Multiplier by chance/sketch.js
+++ All Projects/Classwork/Ball Multiplier by chance/sketch.js
@@ -1,4 +1,4 @@
-let balls = [];
+let MultiBalls = [];
 
 class Ball {
   constructor(x, y) {
@@ -7,29 +7,44 @@ class Ball {
     this.a = random(TWO_PI);
     this.speed = 3;
 
+    this.moveX = cos(this.a) * this.speed;
+    this.moveY = sin(this.a) * this.speed;
 
+    this.fall = 1;
+    this.gravity = 0.1;
+  }
+
+  move() {
+    this.x += this.moveX;
+    this.y += this.moveY;
+    this.y += this.fall;
+    this.fall += this.gravity;
   }
 
 
-  update() {
-
+  show() {
+    fill(255, 0, 0);
+    noStroke()
+    circle(this.x, this.y, 25)
   }
-
 }
 
 
 function setup() {
   createCanvas(400, 400);
+  MultiBalls[0] = new Ball(200, 200);
+
 }
 
 function draw() {
   background(220);
 
-  createBall();
+
+
+  MultiBalls[0].show();
+  MultiBalls[0].move();
 
 }
 
-function createBall() {
-  let ball = new Ball();
-}
+
 
