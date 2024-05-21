6ca26e412b6a3fb02e2472399977525d6b1ee6d8
petericranston <39384415+petericranston@users.noreply.github.com>
2024-05-21 14:31:49 +0100

    Dog not working yet

--- All Projects/Classwork/Loading OBJ Model/sketch.js
+++ All Projects/Classwork/Loading OBJ Model/sketch.js
@@ -1,9 +1,31 @@
+let angle = 1;
+let dog;
+
+function preload() {
+  dog = loadModel('Dog.obj');
+}
+
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(400, 400, WEBGL);
 }
 
 function draw() {
-  background(220);
+  background(0);
+
+  ambientLight(100);
+  directionalLight(255, 255, 255, 0, 0, -1);
+  rotateX(angle);
+  rotateY(angle * 1.4);
+  rotateZ(angle * 0.7);
+
+  // box(100);
+  model(dog)
+  angle += 0.03;
+
+
+
 
 
 }
+
