b57f9b75ca84f84449f9bce8e14cbed62216e453
petericranston <39384415+petericranston@users.noreply.github.com>
2024-05-21 12:19:22 +0100

    Making small 3D project

--- All Projects/Classwork/3D More/sketch.js
+++ All Projects/Classwork/3D More/sketch.js
@@ -1,3 +1,5 @@
+let angle = 1;
+
 function setup() {
   createCanvas(400, 400, WEBGL);
 }
@@ -5,6 +7,15 @@ function setup() {
 function draw() {
   background(220);
 
+  rectMode(CENTER);
+  fill(0, 0, 255);
+  rotateX(angle);
+  rotateY(angle * 0.3);
+  rotateZ(angle * 1.4);
 
+  translate(mouseX, mouseY);
+  box(10, 100, 50)
+
+  angle += 0.07;
 
 }
