084ecf638602673a5a9a7248a54eebb187cffe68
petericranston <39384415+petericranston@users.noreply.github.com>
2024-05-21 18:31:44 +0100

    Creating new program for the blog

---
+++ All Projects/Classwork/Random Walker and Creating Graphics/sketch.js
@@ -0,0 +1,43 @@
+
+let x = 200;
+let y = 200;
+let extraCanvas;
+
+function setup() {
+  createCanvas(400, 400);
+  extraCanvas = createGraphics(400, 400); //Creating an addition canvas: useful if your want some things to trail but not others
+  extraCanvas.clear();
+  background(0);
+}
+
+function keyPressed() {
+  if (key == " ") {
+    extraCanvas.clear();
+  }
+}
+
+function draw() {
+
+
+  //no trails
+  background(0);
+  x += random(-5, 5);
+  y += random(-5, 5);
+  fill(255, 250, 0);
+
+
+  if (mouseIsPressed) {
+    extraCanvas.fill(255, 150, 100);
+    extraCanvas.noStroke();
+    extraCanvas.ellipse(mouseX, mouseY, 60, 60);
+  }
+
+
+  image(extraCanvas, 0, 0)
+
+  stroke(255);
+  rectMode(CENTER);
+  rect(x, y, 25, 25);
+
+
+}
---
+++ _posts/2024-05-21-Random-Walkers-And-Creating-Graphics-post.md
@@ -0,0 +1,10 @@
+---
+title: "Random Walkers and Creating Graphics"
+date: 2024-05-21
+---
+
+## Random Walkers and Creating Graphics
+
+This program does two things at the same time. It uses a random walker to create a square that randomly moves around the screen, without leaving a trail, as well as making it so that the user can draw whatever they want on the screen as if they are drawing a line, which does leave a trail. 
+
+The random walkers aspect is done 
